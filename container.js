f3f97a8252b1a669e5d0cdd0020f7da37d5bfeaa
Cameron Cook <Cameron.Cook@fele.com>
2018-01-03 10:22:10 -0500

    added dependable and set it up and also added lodash

---
+++ container.js
@@ -0,0 +1,16 @@
+const dependable = require("dependable");
+const path = require('path');
+
+const container = dependable.container();
+
+const simpleDependecies = [
+    ['_', 'lodash'],
+];
+
+simpleDependecies.forEach(function (val) {
+    container.register(val[0], function () {
+        return require(val[1]);
+    });
+
+});
+
