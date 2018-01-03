728fdb3c0002f2823f57cf285f8b3b9dfa2fcf1c
Cameron Cook <cameroncook6@yahoo.com>
2018-01-03 11:40:55 -0500

    configure Express is setup

--- server.js
+++ server.js
@@ -6,21 +6,31 @@ const container = require('./container');
 
 
 
-container.resolve(function(users){
-     
+container.resolve(function (users) {
+
     const app = SetupExpress();
 
-    function SetupExpress(){
+    function SetupExpress() {
         const app = express;
         const server = http.createServer(app);
-        server.listen(3000, function(){
+        server.listen(3000, function () {
             console.log("listening on port 3000");
         });
+
+        configureExpress(app);
+        //setup router
+        const router = require('express-promise-router')();
+        users.SetRouteing(router);
     }
-    //setup router
-    const router = require('express-promise-router')();
-    users.SetRouteing(router);
+
 
     app.use(router);
+
+    function configureExpress(app) {
+        app.use(express.static('public'));
+        app.set('view engine', 'ejs');
+        app.use(bodyParser.json());
+        app.use(bodyParser.urlencoded({ extended: true }));
+    }
 
 });
