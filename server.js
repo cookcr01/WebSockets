0a5f8ba6f9cee79154bbd9b13dc8c5574eb76810
Cameron Cook <cameroncook6@yahoo.com>
2018-01-03 12:20:21 -0500

    added more modules

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
+import { globalAgent } from 'http';
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const http = require('http');
+const cookieParser = require('cookie-parser');
+const validator = require('express-validator');
+const session = require('express-session');
+const mongoStore = require('connect-mongo')(session);
+const mongoose = require('mongoose');
+const flash = require('flash');
 const container = require('./container');
 
+
 container.resolve(function (users) {
+
+    mongoose.Promise = global.Promise;
+    mongoose.connect('mongodb://localhost/footballkik',{useMongoClient: true});
 
     const app = SetupExpress();
 
@@ -26,9 +38,20 @@ container.resolve(function (users) {
     
     function ConfigureExpress(app) {
         app.use(express.static('public'));
+        app.use(cookieParser);
         app.set('view engine', 'ejs');
         app.use(bodyParser.json());
         app.use(bodyParser.urlencoded({ extended: true }));
+
+        app.use(validator);
+        app.use(session({
+            secret: 'thisisasecretkey',
+            resave: true,
+            saveInitialized: true,
+            store: new mongoStore({mongooseConnection: mongoose.connection})
+        }));
+
+        app.use(flash());
     }
 
 });
