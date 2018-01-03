fa3a3407926a049a30d85b10486f855dfdf6ec09
Cameron Cook <cameroncook6@yahoo.com>
2018-01-03 10:57:55 -0500

    finished setting up express

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const http = require('http');
+const container = require('./container');
+
+container.resolve(function(){
+     
+    const app = SetupExpress();
+
+    function SetupExpress(){
+        const app = express;
+        const server = http.createServer(app);
+        server.listen(3000, function(){
+            console.log("listening on port 3000");
+        });
+    }
+
+
+});
